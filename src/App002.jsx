5a5ce7b56626241f4bf77df98d4eb34a01807332
s_fan <s_fan@naver.com>
2024-07-17 11:18:26 +0900

    로그인 박스

---
+++ src/App002.jsx
@@ -0,0 +1,54 @@
+// import { useForm } from "react-hook-form";
+
+// export function App() {
+//   const {
+//     register,
+//     handleSubmit,
+//     formState: { errors },
+//   } = useForm();
+
+//   const submitForm = (data) => {
+//     console.log(data);
+//   };
+
+//   console.log(errors);
+
+//   return (
+//     <form autoComplete="off" onSubmit={handleSubmit(submitForm)}>
+//       <h3><bold>Login</bold></h3>
+//       <div>
+//         <label htmlFor="id">Username</label>
+//         <input
+//           type="text"
+//           id="id"
+//           {...register("id", { required: true, maxLength: 10 })}
+//         />
+//         {errors.id && <p>ERROR!!!</p>}
+//       </div>
+//       <div>
+//         <label htmlFor="pw">Password</label>
+//         <input
+//           type="password"
+//           id="pw"
+//           {...register("pw", { required: true, minLength: 5 })}
+//         />
+//         {errors.pw && <p>ERROR!!!</p>}
+//       </div>
+//       <button>Login</button>
+//     </form>
+//   );
+// }
+
+// export default App;
+// 지환
+// 
+// 목샘 로긴박스 ------------------------------------------//
+import {LoginBox} from './components/LoginBox';
+
+export function App(){
+  return (
+    <div>
+      <LoginBox></LoginBox>
+    </div>
+  );
+}
---
+++ src/components/LoginBox.jsx
@@ -0,0 +1,39 @@
+// 지환 import './login.css';
+
+// export function Login(){
+//   return (
+//     <div style={{
+//       display: 'flex', justifyContent: 'center', alignItems: 'center',
+//       width: '100%', height: '100vh', backgroundColor:'rgb(235, 235, 235)'
+//       }}>
+//       <form style={{ display: 'flex', flexDirection: 'column'}}>
+//           <h2>Login</h2>
+//           <input type='text' placeholder='Usename' />
+//           <br/>
+//           <input type='password' placeholder='Password' />
+//           <br/>
+//           <button formAction=''>
+//               Login
+//           </button>
+//           <p>Don't have an account? <a href="#">Register</a></p>
+//       </form>
+//     </div>
+//   );
+// }
+
+// 목샘------------------------------------------------------
+import './LoginBox.css';
+export function LoginBox(){
+  return (    
+    <div class="login-container">
+        <form>
+          <h2>Login</h2>
+          <input type='text' placeholder='Username'/>
+          <br/>
+          <input type='password' placeholder='Password'/>
+          <br/>
+          <button>Login</button>
+        </form>
+    </div>
+  );
+}
